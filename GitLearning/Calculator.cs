f470e8d5b375bc8c59dba6f6b94693d0cf23092d
Tejinder Singh <tejinder94782@gmail.com>
2026-03-26 22:24:28 +0530

    feat: added subtract method into calculator class

--- GitLearning/Calculator.cs
+++ GitLearning/Calculator.cs
@@ -2,8 +2,13 @@
 
 public class Calculator
 {
-   public int Add(int a, int b)
+    public int Add(int a, int b)
     {
         return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
     }
 }
