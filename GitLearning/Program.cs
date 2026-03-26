797556b4556840e283333a1dde378576e4b6d6a4
Tejinder Singh <tejinder94782@gmail.com>
2026-03-26 22:28:21 +0530

    feat: performed addition and subtraction

--- GitLearning/Program.cs
+++ GitLearning/Program.cs
@@ -1,2 +1,10 @@
-﻿Console.WriteLine("Hello, I am learning Git.");
-Console.WriteLine("Integrate Calculator class into main program." );
+﻿using GitLearning;
+
+Console.WriteLine("Hello, I am learning Git.");
+
+Calculator calculator = new();
+int sum = calculator.Add(5, 3);
+Console.WriteLine(sum);
+
+int difference = calculator.Subtract(10, 4);
+Console.WriteLine(difference);
